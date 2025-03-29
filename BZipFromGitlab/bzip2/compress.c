7612ba5e00071c1086ec3da4800a9b5f76612b8b
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2025-03-29 16:50:08 +0100

    rename s to status

--- BZipFromGitlab/bzip2/compress.c
+++ BZipFromGitlab/bzip2/compress.c
@@ -671,47 +671,44 @@ s_code_sel_selCtr[mtfv_i] )
 
 
 /*---------------------------------------------------*/
-void BZ2_compressBlock ( EState* s, Bool is_last_block ) {
-   if (s->nblock > 0) {
-
-     BZ_FINALISE_CRC ( &s->blockCRC );
-      s->combinedCRC = (s->combinedCRC << 1) | (s->combinedCRC >> 31);
-      s->combinedCRC ^= s->blockCRC;
-     if (s->blockNo > 1) {
-       s->numZ = 0;
-     }
-
-     if (s->verbosity >= 2) {
-       VPrintf4( "    block %d: crc = 0x%08x, "
-                "combined CRC = 0x%08x, size = %d\n",
-                s->blockNo, s->blockCRC, s->combinedCRC, s->nblock );
-     }
-
-      BZ2_blockSort ( s );
-   }
-
-   s->zbits = (UChar*) (&((UChar*)s->arr2)[s->nblock]);
-
-   /*-- If this is the first block, create the stream header. --*/
-  if (s->blockNo == 1) {
-    BZ2_bsInitWrite ( s );
-    bsPutUChar ( s, BZ_HDR_B );
-    bsPutUChar ( s, BZ_HDR_Z );
-    bsPutUChar ( s, BZ_HDR_h );
-    bsPutUChar ( s, (UChar)(BZ_HDR_0 + s->blockSize100k) );
-  }
-
-  if (s->nblock > 0) {
+void BZ2_compressBlock ( EState* status, Bool is_last_block ) {
+  
+  if (status->nblock > 0) {
+    BZ_FINALISE_CRC ( &status->blockCRC );
+    status->combinedCRC = (status->combinedCRC << 1) | (status->combinedCRC >> 31);
+    status->combinedCRC ^= status->blockCRC;
+    if (status->blockNo > 1) {
+      status->numZ = 0;
+    }
     
-    bsPutUChar ( s, 0x31 );
-    bsPutUChar ( s, 0x41 );
-    bsPutUChar ( s, 0x59 );
-    bsPutUChar ( s, 0x26 );
-    bsPutUChar ( s, 0x53 );
-    bsPutUChar ( s, 0x59 );
+    if (status->verbosity >= 2) {
+      VPrintf4( "    block %d: crc = 0x%08x, combined CRC = 0x%08x, size = %d\n", status->blockNo, status->blockCRC, status->combinedCRC, status->nblock );
+    }
+    
+    BZ2_blockSort ( status );
+  }
+  
+  status->zbits = (UChar*) (&((UChar*)status->arr2)[status->nblock]);
+  
+  /*-- If this is the first block, create the stream header. --*/
+  if (status->blockNo == 1) {
+    BZ2_bsInitWrite ( status );
+    bsPutUChar ( status, BZ_HDR_B );
+    bsPutUChar ( status, BZ_HDR_Z );
+    bsPutUChar ( status, BZ_HDR_h );
+    bsPutUChar ( status, (UChar)(BZ_HDR_0 + status->blockSize100k) );
+  }
+  
+  if (status->nblock > 0) {
+    bsPutUChar ( status, 0x31 );
+    bsPutUChar ( status, 0x41 );
+    bsPutUChar ( status, 0x59 );
+    bsPutUChar ( status, 0x26 );
+    bsPutUChar ( status, 0x53 );
+    bsPutUChar ( status, 0x59 );
     
     /*-- Now the block's CRC, so it is in a known place. --*/
-    bsPutUInt32 ( s, s->blockCRC );
+    bsPutUInt32 ( status, status->blockCRC );
     
     /*--
      Now a single bit indicating (non-)randomisation.
@@ -722,28 +719,26 @@ void BZ2_compressBlock ( EState* s, Bool is_last_block ) {
      so as to maintain backwards compatibility with
      older versions of bzip2.
      --*/
-    bsW(s,1,0);
+    bsW(status,1,0);
     
-    bsW ( s, 24, s->origPtr );
-    generateMTFValues ( s );
-    sendMTFValues ( s );
+    bsW ( status, 24, status->origPtr );
+    generateMTFValues ( status );
+    sendMTFValues ( status );
   }
-
-
-   /*-- If this is the last block, add the stream trailer. --*/
+  
+  /*-- If this is the last block, add the stream trailer. --*/
   if (is_last_block) {
-    
-    bsPutUChar ( s, 0x17 );
-    bsPutUChar ( s, 0x72 );
-    bsPutUChar ( s, 0x45 );
-    bsPutUChar ( s, 0x38 );
-    bsPutUChar ( s, 0x50 );
-    bsPutUChar ( s, 0x90 );
-    bsPutUInt32 ( s, s->combinedCRC );
-    if (s->verbosity >= 2) {
-      VPrintf1( "    final combined CRC = 0x%08x\n   ", s->combinedCRC );
+    bsPutUChar ( status, 0x17 );
+    bsPutUChar ( status, 0x72 );
+    bsPutUChar ( status, 0x45 );
+    bsPutUChar ( status, 0x38 );
+    bsPutUChar ( status, 0x50 );
+    bsPutUChar ( status, 0x90 );
+    bsPutUInt32 ( status, status->combinedCRC );
+    if (status->verbosity >= 2) {
+      VPrintf1( "    final combined CRC = 0x%08x\n   ", status->combinedCRC );
     }
-    bsFinishWrite ( s );
+    bsFinishWrite ( status );
   }
 }
 
